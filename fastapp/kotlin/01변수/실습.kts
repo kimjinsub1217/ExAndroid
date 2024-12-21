88000e066fccf092b95604f2f3aa039292c1422d
kimjinsub1217 <hikm9985@naver.com>
2024-12-22 01:13:35 +0900

    24/12/22 Commit

---
+++ fastapp/kotlin/01변수/실습.kts
@@ -0,0 +1,16 @@
+// 주석 작성하는 방법
+// 주석 -> 사람만 보기 위해서 작성
+
+// 1. var 변수 선언하기
+var changeYes = 100
+println(changeYes)
+changeYes = 200
+println(changeYes)
+
+// 2. val 변수 선언하기 <- 재할당 x
+//val CHANGE_NO = 100
+//println(CHANGE_NO)
+//CHANGE_NO = 200
+
+val SCHOOL_NAME= "대한민국 학교"
+println(SCHOOL_NAME)
