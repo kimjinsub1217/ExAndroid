32f3342cff340d031fce3c5eb00f30f5e28e173e
김진섭 <76811495+kimjinsub1217@users.noreply.github.com>
2025-01-10 21:38:51 +0900

    🧸add :ConfigurationChange

---
+++ ConfigurationChange/app/src/main/java/com/jinsub/configurationchange/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.jinsub.configurationchange
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.jinsub.configurationchange.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    // Log 태그
+    private val TAG = "MainActivityLifecycle"
+    private lateinit var binding: ActivityMainBinding
+    private var counter = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        Log.d(TAG, "onCreate")
+        binding.incrementButton.setOnClickListener {
+            counter++
+            binding.counterText.text = "Counter: $counter"
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+}
