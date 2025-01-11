08c24b1224c275096f12bb16631b99649de7f30b
김진섭 <76811495+kimjinsub1217@users.noreply.github.com>
2025-01-11 20:45:17 +0900

    ✨feat: 가로모드 회전 UI 작성

--- ExGeoQuiz/app/src/main/java/com/jinsub/exgeoquiz/MainActivity.kt
+++ ExGeoQuiz/app/src/main/java/com/jinsub/exgeoquiz/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.jinsub.exgeoquiz
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -8,6 +9,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.jinsub.exgeoquiz.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,12 +25,13 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_asia, true),
     )
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
         binding.trueButton.setOnClickListener {
             checkAnswer(userAnswer = true)
         }
@@ -63,5 +67,30 @@ class MainActivity : AppCompatActivity() {
             R.string.incorrect_toast
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 }
