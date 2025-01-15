00071309478033b643047b2ef365e94ee9c42c86
김진섭 <76811495+kimjinsub1217@users.noreply.github.com>
2025-01-15 23:28:50 +0900

    🧸add photoGallery

--- photoGallery/app/build.gradle.kts
+++ photoGallery/app/build.gradle.kts
@@ -1,16 +1,14 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("kotlin-kapt")
-
 }
 
 android {
-    namespace = "com.jinsub.beatbox"
+    namespace = "com.jinsub.photogallery"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.jinsub.beatbox"
+        applicationId = "com.jinsub.photogallery"
         minSdk = 28
         targetSdk = 35
         versionCode = 1
@@ -35,9 +33,6 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    buildFeatures {
-        dataBinding = true
-    }
 }
 
 dependencies {
@@ -47,13 +42,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.recyclerview)
-
-
     testImplementation(libs.junit)
-    testImplementation (libs.mockito.core)
-
-    testImplementation(libs.junit.jupiter)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
