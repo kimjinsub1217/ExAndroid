c1b0b390ca4fa105f7980fc686c249e074e02a61
김진섭 <76811495+kimjinsub1217@users.noreply.github.com>
2025-01-17 18:59:30 +0900

    🛠refactor dataBinding으로 리팩토링

--- viewModel/app/src/main/java/com/jinsub/viewmodel/MainFragment.kt
+++ viewModel/app/src/main/java/com/jinsub/viewmodel/MainFragment.kt
@@ -5,15 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.jinsub.viewmodel.databinding.FragmentMainBinding
+import com.jinsub.viewmodel.BR.myViewModel
 
 
 class MainFragment : Fragment() {
 
-    private var _binding: FragmentMainBinding? = null
-    private val binding get() = _binding!!
+    lateinit var binding: FragmentMainBinding
 
     companion object {
         fun newInstance() = MainFragment()
@@ -25,30 +25,33 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_main, container, false
+        )
+        binding.setLifecycleOwner(this)
         return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ViewModel::class.java)
+        binding.setVariable(myViewModel, viewModel)
 //        binding.resultText.text = viewModel.getResult().toString()
+//        val resultObserver = Observer<Float> { result ->
+//            binding.resultText.text = result.toString()
+//        }
 
-        val resultObserver = Observer<Float> { result ->
-            binding.resultText.text = result.toString()
-        }
 
-
-        viewModel.getResult().observe(viewLifecycleOwner, resultObserver)
-
-        binding.convertButton.setOnClickListener {
-            if (binding.dollarText.text.isNotEmpty()) {
-                viewModel.setAmount(binding.dollarText.text.toString())
-//                binding.resultText.text = viewModel.getResult().toString()
-            } else {
-                binding.resultText.text = "No Value"
-            }
-        }
+//        viewModel.getResult().observe(viewLifecycleOwner, resultObserver)
+//
+//        binding.convertButton.setOnClickListener {
+//            if (binding.dollarText.text.isNotEmpty()) {
+//                viewModel.setAmount(binding.dollarText.text.toString())
+////                binding.resultText.text = viewModel.getResult().toString()
+//            } else {
+//                binding.resultText.text = "No Value"
+//            }
+//        }
     }
 
 }
--- viewModel/app/src/main/java/com/jinsub/viewmodel/ViewModel.kt
+++ viewModel/app/src/main/java/com/jinsub/viewmodel/ViewModel.kt
@@ -4,19 +4,33 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ViewModel : ViewModel() {
+
+//    private var dollarText = ""
+
+//    private var result: Float = 0f
+
+//    private var result: MutableLiveData<Float> = MutableLiveData()
     private val usd_to_eu_rate = 0.74f
-    private var dollarText = ""
+    var dollarValue: MutableLiveData<String> = MutableLiveData()
+    var result: MutableLiveData<Float> = MutableLiveData()
 
-    //    private var result: Float = 0f
-    private var result: MutableLiveData<Float> = MutableLiveData()
+//    fun setAmount(value: String) {
+//        this.dollarText = value
+////        result = value.toFloat() * usd_to_eu_rate
+//        result.value = value.toFloat() * usd_to_eu_rate
+//    }
+//
+//    fun getResult(): MutableLiveData<Float> {
+//        return result
+//    }
 
-    fun setAmount(value: String) {
-        this.dollarText = value
-//        result = value.toFloat() * usd_to_eu_rate
-        result.value = value.toFloat() * usd_to_eu_rate
-    }
-
-    fun getResult(): MutableLiveData<Float> {
-        return result
+    fun convertValue() {
+        dollarValue.let {
+            if (!it.value.equals("")) {
+                result.value = it.value?.toFloat()?.times(usd_to_eu_rate)
+            } else {
+                result.value = 0f
+            }
+        }
     }
 }
