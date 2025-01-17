88dc1f07fb492992114b0634237e3abd57bfd9b5
김진섭 <76811495+kimjinsub1217@users.noreply.github.com>
2025-01-17 18:09:41 +0900

    ✨feat LiveData 넣어보기

--- viewModel/app/src/main/java/com/jinsub/viewmodel/MainFragment.kt
+++ viewModel/app/src/main/java/com/jinsub/viewmodel/MainFragment.kt
@@ -5,11 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.jinsub.viewmodel.databinding.FragmentMainBinding
 
 
-class MainFragment  : Fragment() {
+class MainFragment : Fragment() {
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
@@ -31,12 +32,19 @@ class MainFragment  : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ViewModel::class.java)
-        binding.resultText.text = viewModel.getResult().toString()
+//        binding.resultText.text = viewModel.getResult().toString()
+
+        val resultObserver = Observer<Float> { result ->
+            binding.resultText.text = result.toString()
+        }
+
+
+        viewModel.getResult().observe(viewLifecycleOwner, resultObserver)
 
         binding.convertButton.setOnClickListener {
             if (binding.dollarText.text.isNotEmpty()) {
                 viewModel.setAmount(binding.dollarText.text.toString())
-                binding.resultText.text = viewModel.getResult().toString()
+//                binding.resultText.text = viewModel.getResult().toString()
             } else {
                 binding.resultText.text = "No Value"
             }
--- viewModel/app/src/main/java/com/jinsub/viewmodel/ViewModel.kt
+++ viewModel/app/src/main/java/com/jinsub/viewmodel/ViewModel.kt
@@ -1,18 +1,22 @@
 package com.jinsub.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class ViewModel: ViewModel() {
+class ViewModel : ViewModel() {
     private val usd_to_eu_rate = 0.74f
     private var dollarText = ""
-    private var result: Float = 0f
+
+    //    private var result: Float = 0f
+    private var result: MutableLiveData<Float> = MutableLiveData()
 
     fun setAmount(value: String) {
         this.dollarText = value
-        result = value.toFloat() * usd_to_eu_rate
+//        result = value.toFloat() * usd_to_eu_rate
+        result.value = value.toFloat() * usd_to_eu_rate
     }
 
-    fun getResult(): Float? {
+    fun getResult(): MutableLiveData<Float> {
         return result
     }
 }
